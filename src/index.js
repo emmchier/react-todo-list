38a959702153876649f68a38785e658a6d88caaa
emmchier <emmchierchie@gmail.com>
2021-05-20 11:23:57 -0300

    creating reducer and showing first item list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import { LetsTodoIt } from './LetsTodoIt';
 
+// main styles
+import './styles/_main.scss';
+
 ReactDOM.render(
     <LetsTodoIt />,
   document.getElementById('root')
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+import { todoReducer } from '../reducers/todoReducer';
+
+const initialState = [{
+    id: new Date().getTime(),
+    desc: 'task 1',
+    done: false
+}];
 
 export const HomeScreen = () => {
+
+    const [ todos ] = useReducer( todoReducer, initialState );
+    console.log(todos);
+
     return (
         <div className="container">
-            <h2>TODO LIST</h2>
+            <h2>TODO LIST ({ todos.length })</h2>
+
+            <ul className="home__todoList list-group list-group-flush">
+
+                {
+                    todos.map( (task, i) => (
+                        <li
+                            key={ task.id }
+                            className="list-group-item"
+                        >
+                            { i + 1 } { task.desc }
+                        </li>
+                    ))
+                }
+
+            </ul>
         </div>
     )
 }
